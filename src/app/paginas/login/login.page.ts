5a75ef10af4e0bb9ecd18def1d41aad955a4e7ef
Oswaldo Garces <oswaldogarces98@gmail.com>
2022-03-14 16:45:49 -0500

    Primer commit

---
+++ src/app/paginas/login/login.page.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  async login(email, password){
+    try{
+      
+    }catch(error){
+
+    }
+  }
+
+}
